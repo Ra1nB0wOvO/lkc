8a0b4b39953e18048c0f29a569817f54f9e02627
zcl <144252813+AKAconglin666@users.noreply.github.com>
2024-11-09 17:50:35 +0800

    Add files via upload

---
+++ OrderController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Order;
+import com.example.demo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+}
